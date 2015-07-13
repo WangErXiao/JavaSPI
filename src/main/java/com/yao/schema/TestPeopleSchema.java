6fefd53ad17a62256f29c0bf12f2231ccd69c2ac
WangErXiao <863579016@qq.com>
2015-07-13 17:38:13 +0800

    spring schema

---
+++ src/main/java/com/yao/schema/TestPeopleSchema.java
@@ -0,0 +1,23 @@
+package com.yao.schema;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by yao on 15/7/13.
+ */
+public class TestPeopleSchema {
+    public static void main(String[]args){
+
+        ApplicationContext context=
+                new ClassPathXmlApplicationContext("classpath*:schema/peoples.xml");
+
+        People people0= context.getBean("people0",People.class);
+        People people1= context.getBean("people1",People.class);
+        People people2= context.getBean("people2",People.class);
+        System.out.println(people0.getId()+ " "+people0.getName()+" "+people0.getAge());
+        System.out.println(people1.getId()+ " "+people1.getName()+" "+people1.getAge());
+        System.out.println(people2.getId()+ " "+people2.getName()+" "+people2.getAge());
+
+    }
+}
